f363755783819a9ec7f580d4359a65045ef13176
idowuomidoyin <idowuomidoyin@yahoo.com>
2023-05-25 16:30:52 -0700

    monty

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * auth: omidoyin
+ *f_nop- nothing
+ *@head: stack head
+ *@counter: line_number
+ *Return: no return
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
